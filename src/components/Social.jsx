a24c529d779acd655ebbedc35acfa18634bf6b7d
Duamajeed00 <mduaa0976@gmail.com>
2024-11-09 16:34:18 +0500

    links

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Link from "next/link";
-import {FaGithub, FaLinkedinIn, FaYoutube, FaTwitter} from "react-icons/fa";
+import {FaGithub, FaLinkedinIn, FaTwitter} from "react-icons/fa";
 
 const socials = [
-    {icon: <FaGithub />, path:""},
-    {icon: <FaLinkedinIn />, path:""},
-    {icon: <FaYoutube />, path:""},
-    {icon: <FaTwitter />, path:""},
+    {icon: <FaGithub />, path:"https://github.com/Duamajeed00"},
+    {icon: <FaLinkedinIn />, path:"https://www.linkedin.com/in/dua-majeed-6b99482b4/"},
+    // {icon: <FaYoutube />, path:""},
+    {icon: <FaTwitter />, path:" https://x.com/majeed_dua91585?t=aJNRaTRviMk3nPtR0ffnoA&s=08"},
 ]
 
 const Social = ({containerStyles, iconStyles}) => {
